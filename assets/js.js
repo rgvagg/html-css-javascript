d13e109074909938e37dfb3d129838da19ccaa94
rgv <r@gm.com>
2013-07-25 17:35:06 +0530

    Lightbox background transparency, position absolute(left right top bottom), in textfield onclick text value disappears, rgba

--- assets/js.js
+++ assets/js.js
@@ -1,19 +1,22 @@
 // JavaScript Document
-var a,b=300,c=10,d,x;
+var a,b=255,c=10,d,x1=300,x;
 //a=1100;
 a=1000;
 function func1()
 {	//start:
 		a-=5;
+		b-=3;
+		c+=5;
 	//document.write(a+b+c);
 		//document.getElementById("temp").style.borderRadius=b+"px";
 	document.getElementById("temp").style.width=(a+"px");
 	//document.getElementById("temp1").style.display="block";	
-	//document.getElementById("temp").color.rgb=(100, 500, 900);
+	document.getElementById("temp").style.background="rgb("+b+", "+c+", "+b+")";
 	document.getElementById("temp").style.opacity=(a*0.001);
 	//document.getElementById("temp").innerHTML=("<p>"+a+"</p>");
 	if(a<300)
-	{	
+	{	//c=100;
+	c+=5;
 		document.getElementById("temp").style.display="none";
 
 	//a+=5;
@@ -24,11 +27,15 @@ function func1()
 
 	document.getElementById("temp1").style.display="block";
 	//a=a+10;	
-	b+=5;
-	document.getElementById("temp1").style.width=(b+"px");
-	document.getElementById("temp1").style.opacity=(b*0.001);
+		document.getElementById("temp1").style.background="rgb("+(50)+","+((c)*0.05)+", "+(100)+")";
 
-	if(b>1000)
+	x1+=5;
+	document.getElementById("temp1").style.width=(x1+"px");
+
+	document.getElementById("temp1").style.opacity=(x1*0.001);
+	//document.getElementById("temp1").innerHTML=c;
+
+	if(x1>1000)
 	{	document.getElementById("temp1").style.display="none";
 	//a=1100;
 	//repeat: goto start;
